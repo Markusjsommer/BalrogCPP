58dbe575069cc0a7ea617be5e832bd32b56f461d
Markus Sommer <markusjsommer@gmail.com>
2021-01-19 14:47:51 -0500

    add MMseqs2 support

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <cctype>
+#include <cstdio>
 #include "cxxopts.hpp"
 #include "FastaReader.h"
 #include "GeneFinder.h"
+#include <cmrc/cmrc.hpp>
+
+CMRC_DECLARE(cmakeresources);
 
 int main(int argc, char* argv[]) {
     // parse command line options
-    cxxopts::Options options("balrog", "balrog is a prokaryotic gene finder based on a temporal convolutional network");
+    cxxopts::Options options("Balrog", "Balrog is a prokaryotic gene finder based on a temporal convolutional network");
     options.add_options()
             ("i, in", "Path to input fasta or gzipped fasta", cxxopts::value<std::string>())
             ("o, out", "Path to output annotation", cxxopts::value<std::string>())
-            ("g, gene-model", "Path to pretrained LibTorch gene model", cxxopts::value<std::string>())
-            ("t, TIS-model", "Path to pretrained LibTorch TIS model", cxxopts::value<std::string>())
             ("max-overlap", "Maximum allowable overlap between genes in nucleotides", cxxopts::value<int>()->default_value("60"))
-            ("min-length", "Minimum allowable gene length in nucleotides", cxxopts::value<int>()->default_value("60"))
+            ("min-length", "Minimum allowable gene length in nucleotides", cxxopts::value<int>()->default_value("90"))
             ("table", "Nucleotide to amino acid translation table. 11 for most bacteria/archaea, 4 for Mycoplasma/Spiroplasma.",
                     cxxopts::value<int>()->default_value("11"))
             ("max-connections", "Maximum number of forward connections in the directed acyclic graph used to find a set of coherent genes in each genome.",
@@ -22,8 +24,10 @@ int main(int argc, char* argv[]) {
                     cxxopts::value<int>()->default_value("200"))
             ("TIS-batch-size", "Batch size for the temporal convolutional network used to score TIS.",
                     cxxopts::value<int>()->default_value("5000"))
-            ("verbose", "Verbose output", cxxopts::value<bool>()->default_value("true"))
-            ("h,help", "Print balrog usage")
+            ("verbose", "Verbose output, set --verbose=false to suppress output text", cxxopts::value<bool>()->default_value("true"))
+            ("mmseqs", "Use MMseqs2 to reduce false positive rate, set --mmseqs=false to run without mmseqs", cxxopts::value<bool>()->default_value("true"))
+            ("clear-cache", "Force MMseqs2 to remake index, set --clear-cache=true to remake mmseqs reference database and index files", cxxopts::value<bool>()->default_value("false"))
+            ("h, help", "Print Balrog usage")
             ;
     auto result = options.parse(argc, argv);
 
@@ -40,34 +44,135 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
 
+
+    // PREPARE MODELS AND DATA
+    // open embedded filesystem to load models and data
+    auto fs = cmrc::cmakeresources::get_filesystem();
+
+    // get path to temp directory
+    std::string tmp_dir;
+    char* tmp = std::tmpnam(nullptr);
+    int lastslash = ((std::string) tmp).rfind('/');
+    tmp_dir = ((std::string) tmp).substr(0, lastslash);
+    if (result["verbose"].as<bool>()){
+        std::cout << "Saving temp files to " + tmp_dir << std::endl;
+    }
+
+
     // load LibTorch jit traced gene model
     if (result["verbose"].as<bool>()){
         std::cout << "Importing gene model..." << std::endl;
     }
-    std::string model_path = result["gene-model"].as<std::string>();
+    // write gene model from embedded filesystem to tmp file
+    std::ofstream stream;
+    char* tmp_genemodel_path = std::tmpnam(nullptr);
+    auto rc = fs.open("gene_model_v1.0.pt");
+    stream.open(tmp_genemodel_path);
+    auto it = rc.begin();
+    while (it != rc.end()){
+        stream << *it;
+        it ++;
+    }
+    stream.close();
+    // load gene model using jit
     torch::jit::script::Module gene_model;
     try {
-        gene_model = torch::jit::load(model_path);
+        gene_model = torch::jit::load(tmp_genemodel_path);
     }
     catch (const c10::Error& e) {
         std::cerr << "error loading the LibTorch gene model\n";
+        remove(tmp_genemodel_path);
         return -1;
     }
+    // clean up temp file
+    remove(tmp_genemodel_path);
 
     // load LibTorch jit traced TIS model
     if (result["verbose"].as<bool>()){
         std::cout << "Importing TIS model..." << std::endl;
     }
-    std::string model_path_TIS = result["TIS-model"].as<std::string>();
+    char* tmp_TISmodel_path = std::tmpnam(nullptr);
+    rc = fs.open("TIS_model_v1.0.pt");
+    stream.open(tmp_TISmodel_path);
+    it = rc.begin();
+    while (it != rc.end()){
+        stream << *it;
+        it ++;
+    }
+    stream.close();
+    // load TIS model using jit
     torch::jit::script::Module TIS_model;
     try {
-        TIS_model = torch::jit::load(model_path_TIS);
+        TIS_model = torch::jit::load(tmp_TISmodel_path);
     }
     catch (const c10::Error& e) {
         std::cerr << "error loading the LibTorch TIS model\n";
+        remove(tmp_TISmodel_path);
         return -1;
     }
+    // clean up temp file
+    remove(tmp_TISmodel_path);
 
+    // load reference gene sequence
+    if (result["mmseqs"].as<bool>()){
+        std::string ref_fasta_path = tmp_dir + "/reference_genes.fasta";
+        std::string ref_db_path = tmp_dir + "/reference_genes.db";
+        std::string ref_index_path = tmp_dir + "/balrog_mmseqs_index";
+
+        // use precomputed mmseqs database to save time on multiple runs
+        std::ifstream infile(ref_index_path);
+        if (result["verbose"].as<bool>() and (not result["clear-cache"].as<bool>())) {
+            std::cout << "Found MMseqs2 index at " + ref_index_path << std::endl;
+        }
+
+        // create new mmseqs index if needed
+        if ((not infile.good()) or (result["clear-cache"].as<bool>())) {
+            if (result["verbose"].as<bool>()) {
+                std::cout << "Loading reference genes..." << std::endl;
+            }
+            char *tmp_reference_path = std::tmpnam(nullptr);
+            rc = fs.open("reference_genes.tar.gz");
+            stream.open(tmp_reference_path);
+            it = rc.begin();
+            while (it != rc.end()) {
+                stream << *it;
+                it++;
+            }
+            stream.close();
+            // unzip reference genes
+            std::string command = "tar -xf " + (std::string) tmp_reference_path + " -C " + tmp_dir;
+            int status = std::system(command.c_str());
+            if (status != 0) {
+                std::cerr << "error loading reference genes\n";
+                return -1;
+            }
+            // clean temp file
+            remove(tmp_reference_path);
+
+            // create mmseqs reference database and index
+            command = "mmseqs createdb " + ref_fasta_path + " " + ref_db_path;
+            if (not result["verbose"].as<bool>()){
+                command += " -v 0";
+            }
+            status = std::system(command.c_str());
+            if (status != 0) {
+                std::cerr << "error creating mmseqs database\n";
+                return -1;
+            }
+            command = "mmseqs createindex " + ref_db_path + " " + ref_index_path;
+            if (not result["verbose"].as<bool>()){
+                command += " -v 0";
+            }
+            status = std::system(command.c_str());
+            if (status != 0) {
+                std::cerr << "error creating mmseqs index\n";
+                return -1;
+            }
+        }
+    }
+
+
+    // PREDICT GENES
     // read fasta
     if (result["verbose"].as<bool>()){
         std::cout << "Reading fasta..." << std::endl;
@@ -79,7 +184,7 @@ int main(int argc, char* argv[]) {
     FastaReader io;
     io.read_fasta(in_path, seq_vec, contigname_vec);
 
-    // capitalize all nucelotides
+    // capitalize all nucleotides
     for (auto &seq : seq_vec){
         for(auto &c: seq){
             c = toupper(c);
@@ -91,6 +196,7 @@ int main(int argc, char* argv[]) {
     std::vector<std::vector<bool>> gene_strand_vec;
     std::vector<std::vector<std::string>> gene_nucseq_vec;
     std::vector<std::vector<std::string>> gene_protseq_vec;
+    std::vector<std::vector<double>> gene_score_vec;
 
     int i = 0;
     for (std::string seq : seq_vec){
@@ -105,27 +211,31 @@ int main(int argc, char* argv[]) {
         std::vector<bool> gene_strand;
         std::vector<std::string> gene_nucseq;
         std::vector<std::string> gene_protseq;
-
+        std::vector<double> gene_score;
 
         gf.find_genes(seq,
                       gene_coord,
                       gene_strand,
                       gene_nucseq,
                       gene_protseq,
+                      gene_score,
                       table,
                       result["min-length"].as<int>(),
                       result["max-overlap"].as<int>(),
                       result["verbose"].as<bool>(),
                       result["gene-batch-size"].as<int>(),
-                      result["TIS-batch-size"].as<int>());
-
+                      result["TIS-batch-size"].as<int>(),
+                      result["mmseqs"].as<bool>());
 
         gene_coord_vec.emplace_back(gene_coord);
         gene_strand_vec.emplace_back(gene_strand);
         gene_nucseq_vec.emplace_back(gene_nucseq);
         gene_protseq_vec.emplace_back(gene_protseq);
+        gene_score_vec.emplace_back(gene_score);
     }
 
+
+    // OUTPUT PREDICTIONS
     // export genes to gff annotation file
     if (result["verbose"].as<bool>()) {
         std::cout << "Writing predicted genes to " << result["out"].as<std::string>() << std::endl;
@@ -161,7 +271,7 @@ int main(int argc, char* argv[]) {
             } else{
                 out << "-\t";
             }
-            out << "0\tinference=ab initio prediction:balrog;product=hypothetical protein" << std::endl;
+            out << "0\tinference=ab initio prediction:Balrog;product=hypothetical protein" << std::endl;
         }
     }
     out.close();
